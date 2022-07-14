952a317ea996ca625acdc2ff723681f4a8ff7b3f
Lucas Garcia <lucas.garcia_021@hotmail.com>
2022-07-13 23:13:51 -0300

    feature: creating project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    componente aqui
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
